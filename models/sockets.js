647120818963449b36842252301b153864fc1269
Gregory Palacios <gregpalacios26@gmail.com>
2021-04-10 19:59:34 -0500

    Primer commit mapas socket server

---
+++ models/sockets.js
@@ -0,0 +1,35 @@
+const Marcadores = require("./marcadores");
+
+class Sockets {
+
+    constructor(io) {
+        this.io = io;
+
+        this.marcadores = new Marcadores();
+
+        this.socketEvents();
+    }
+
+    socketEvents() {
+        // On connection
+        this.io.on('connection', (socket) => {
+
+            console.log('Cliente conectado');
+
+            socket.emit('marcadores-activos', this.marcadores.activos);
+
+            socket.on('marcador-nuevo', (marcador) => {
+                this.marcadores.agregarMarcador(marcador);
+                socket.broadcast.emit('marcador-nuevo', marcador);
+            });
+
+            socket.on('marcador-actualizado', (marcador) => {
+                this.marcadores.actualizarMarcador(marcador);
+                socket.broadcast.emit('marcador-actualizado', marcador);
+            });
+        });
+    }
+
+}
+
+module.exports = Sockets;
